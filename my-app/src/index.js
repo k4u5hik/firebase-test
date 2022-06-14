6f098911e5a682f739b5b84b62cd1369064df62a
Kaushik Chemburkar <attherate@gmail.com>
2022-06-14 18:46:38 +1000

    npm install @apollo/client graphql - New Tutorial

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// https://rickandmortyapi.com/graphql/
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
