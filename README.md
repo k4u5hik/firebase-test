a011d8e46cdfdafb9be099e03f7b404cb911d24b
Kaushik Chemburkar <attherate@gmail.com>
2022-06-14 18:53:09 +1000

    React With GraphQL (Apollo Client)

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # firebase-test
-Testing out firebase 
+
+Testing out firebase. Comments made in my second and third commit message.
+After attempting to deploy firebase, I was unable to get the app working. I started another [video](https://www.youtube.com/watch?v=gAbIQx26wSI) on React With GraphQL (Apollo Client).
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client"
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache()
+});
 
 // https://rickandmortyapi.com/graphql/
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
