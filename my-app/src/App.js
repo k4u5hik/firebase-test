d79a2ccb3c0bde3deffd2d9ad8c755e9f62b1b01
Kaushik Chemburkar <attherate@gmail.com>
2022-06-14 19:32:27 +1000

    console.log({error,loading, data})

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import CharactersList from './pages/CharactersList';
 
 function App() {
   return (
     <div className="App">
-
+      <CharactersList/>
     </div>
   );
 }
--- my-app/src/pages/CharactersList.js
+++ my-app/src/pages/CharactersList.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
+import {useQuery, gql} from '@apollo/client';
+
+const GET_CHARACTERS = gql
+`
+query {
+    characters {
+        results{
+            id
+            name
+            image
+        }
+    }
+}
+`;
 
 export default function CharactersList() {
+
+    const {error,data,loading} = useQuery(GET_CHARACTERS)
+    console.log({error,loading, data})
+
   return (
-    <div>CharactersList</div>
+    <div></div>
   )
 }
